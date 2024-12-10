fdd5b32eea9e3c779e3264f255ae2ef62dcd5687
Christian Madufor <chrismadufor@gmail.com>
2024-12-10 16:52:07 +0000

    updates

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -4,9 +4,10 @@ import { reset, setUser } from "@/redux/features/controlsSlice";
 import { useAppDispatch } from "@/redux/hooks";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { setCategory } from "@/redux/features/playSlice";
 
 export default function Home() {
   const router = useRouter();
@@ -40,14 +41,34 @@ export default function Home() {
   // const onChange = (event: any) => {
   //   dispatch(setUser(event.target.value))
   // }
+  const [categoryData, setCategoryData] = useState("");
+  const onCategoryChange = (event: any) => {
+    setCategoryData(event.target.value);
+  };
+  const startGame = () => {
+    if (!categoryData) {
+      alert("Fill all fields");
+    }
+    let data: string = categoryData.toUpperCase().trim();
+    dispatch(setCategory(data));
+    router.push("/game/question")
+  }
 
   return (
     <main className="">
       {role === "admin" && <div className="h-screen w-full flex flex-col items-center justify-center bg-blue-800 pl-20">
         <p className="text-5xl">Who Wants To Win Some Money?</p>
-        <Link href="/game/question">
-          <button className="orange-bg py-3 px-8 mt-8">Start Game</button>
-        </Link>
+        <div>
+          <input
+            value={categoryData}
+            onChange={onCategoryChange}
+            type="text"
+            placeholder="Round"
+            className="w-full h-10 md:h-12 mb-3 border text-gray-700 pl-3"
+            required
+          />
+          <button onClick={startGame} className="orange-bg py-3 px-8 mt-8">Start Game</button>
+        </div>
       </div>}
       {/* <div className="fixed top-0 right-0">
         <select name="role" onChange={onChange} id="role" className="h-8 w-20 bg-blue-900 text-white pl-2">
--- app/game/question/components/AdBlock.tsx
+++ app/game/question/components/AdBlock.tsx
@@ -3,15 +3,16 @@ import React from 'react'
 
 export default function AdBlock() {
   return (
-    <div className='h-full relative bg-green-100 mb-5'>
-      <Image 
+    <div className='h-full relative mb-5'>
+      <p className='font-semibold text-3xl text-center'>COC Lugbe</p>
+      {/* <Image 
       priority={true}
       src={"/assets/shelta-banner2.png"}
       fill
       className="object-cover object-center"
       // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       alt="chart"
-      />
+      /> */}
     </div>
   )
 }
